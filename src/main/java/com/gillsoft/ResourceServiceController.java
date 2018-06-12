f322e70a755a41b540bd88c87ea044a2c7c8eb7e
kashpur <a.kashpur@busfor.com>
2018-06-12 17:10:03 +0300

    Проэкт ресурса ДОПАС

---
+++ src/main/java/com/gillsoft/ResourceServiceController.java
@@ -0,0 +1,47 @@
+package com.gillsoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gillsoft.abstract_service.AbstractResourceService;
+import com.gillsoft.model.Method;
+import com.gillsoft.model.Ping;
+import com.gillsoft.model.Resource;
+
+@RestController
+public class ResourceServiceController extends AbstractResourceService {
+
+	@Override
+	public Resource getInfo() {
+		Resource resource = new Resource();
+		resource.setCode("DOPAS");
+		resource.setName("Днепропетровский ОПАС");
+		return resource;
+	}
+
+	@Override
+	public List<Method> getMethods() {
+		List<Method> methods = new ArrayList<>();
+		addMethod(methods, "Resource activity check", Method.PING);
+		addMethod(methods, "Information about resource", Method.INFO);
+		addMethod(methods, "Available methods", Method.METHOD);
+		return methods;
+	}
+	
+	private void addMethod(List<Method> methods, String name, String url) {
+		Method method = new Method();
+		method.setName(name);
+		method.setUrl(url);
+		methods.add(method);
+	}
+
+	@Override
+	public Ping ping(String id) {
+		Ping ping = new Ping();
+		ping.setId(id);
+		return ping;
+	}
+
+}
